2b00b3f15dad374768476160f84a3afe72896a8b
mahditakrusta <mahditakrusta@gmail.com>
2026-07-29 04:13:18 +0330

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -15,7 +15,7 @@ export default function Header() {
           {/* Logo */}
          <Link href="/" className="flex items-center gap-2 group">
   <img
-    src="/logo.jpg"
+    src="/logo.png"
     alt="رَمل"
     className="w-10 h-10 md:w-12 md:h-12 object-contain rounded-full shadow-lg group-hover:shadow-xl transition-shadow"
   />
